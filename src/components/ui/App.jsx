13c2d3330320c012f08245633e6fbe8f4544edc1
Jon Linnell <linnell.jon@gmail.com>
2018-03-15 21:09:53 +0000

    Add Bootstrap grid outline

--- src/components/ui/App.jsx
+++ src/components/ui/App.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 import '../../styles/App.css'
 
-const App = () => <div>test</div>
+const App = () =>
+  <div class='container-fluid'>
+    <div class='row py-4'>
+      <div class='col-sm col-1'>
+        Col 1
+      </div>
+      <div class='col-sm col-2'>
+        Col 2
+      </div>
+      <div class='col-sm col-3'>
+        Col 3
+      </div>
+    </div>
+  </div>
 
 export default App

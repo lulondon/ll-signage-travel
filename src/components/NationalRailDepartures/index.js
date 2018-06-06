eedb4c973c0d0c06ef695220d62eede7cbaa49d8
Jon Linnell <linnell.jon@gmail.com>
2018-06-06 22:11:08 +0100

    Slightly stagger NR refresh intervals

--- src/components/NationalRailDepartures/index.js
+++ src/components/NationalRailDepartures/index.js
@@ -7,6 +7,13 @@ import DepartureBoardView from './component'
 
 const { darwinApiProxy, darwinToken, refreshInterval } = rail
 
+const randomRefreshInterval = () => {
+  const min = refreshInterval - 1500
+  const max = refreshInterval + 1500
+  // eslint-disable-next-line no-mixed-operators
+  return Math.random() * (min - max) + min
+}
+
 class NationalRailDepartures extends Component {
   constructor(props) {
     super(props)
@@ -33,7 +40,8 @@ class NationalRailDepartures extends Component {
   componentDidMount() {
     this.loadData()
 
-    const timer = setInterval(() => this.loadData(), refreshInterval || 120000)
+    // eslint-disable-next-line no-mixed-operators
+    const timer = setInterval(() => this.loadData(), randomRefreshInterval())
     this.setState({ timer })
   }
 
